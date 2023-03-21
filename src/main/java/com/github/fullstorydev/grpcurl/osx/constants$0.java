f28c8ad6cc38043679b24bbf68ef5f7ff0b2edd3
eric.r.driggs <eric.r.driggs@disneystreaming.com>
2023-03-21 11:35:00 -0700

    lint

--- src/main/java/com/github/fullstorydev/grpcurl/osx/constants$0.java
+++ src/main/java/com/github/fullstorydev/grpcurl/osx/constants$0.java
@@ -3,10 +3,7 @@
 package com.github.fullstorydev.grpcurl.osx;
 
 import java.lang.invoke.MethodHandle;
-import java.lang.invoke.VarHandle;
-import java.nio.ByteOrder;
 import java.lang.foreign.*;
-import static java.lang.foreign.ValueLayout.*;
 class constants$0 {
 
     static final FunctionDescriptor cmain$FUNC = FunctionDescriptor.of(Constants$root.C_POINTER$LAYOUT,
